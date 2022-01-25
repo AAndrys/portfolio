9882e9d43c1e31fd8c0d618bf37e94412cd2d3a3
Andrys <aadam606@gmail.com>
2022-01-26 00:53:31 +0100

    chore: changes

--- src/components/sections/aboutMe/index.jsx
+++ src/components/sections/aboutMe/index.jsx
@@ -24,21 +24,17 @@ const AboutMe = ({}) => {
       </div>
       <div ref={wrapperRef} className="about-wrapper">
         <p>
-          Hello, my name is Adam. I am 22 years old and I am from Poland. As you
-          can see I am a JavaScript developer, and this is what I love so much!
-          Too much time spent on learning, a lot of bad code and failures caused
-          me resistant on defeat. Working with a large amount of lines code,
-          experience caused me increasing more professionalism.
+          Hello, my name is Adam. I am 22 years old and I am from Poland. As you can see I am a
+          JavaScript developer, and this is what I love so much! Too much time spent on learning, a
+          lot of bad code and failures caused me resistant on defeat. Working with a large amount of
+          lines code, experience caused me increasing more professionalism.
           <br />
           <strong>Programming is my hooby and I love it!</strong>{" "}
         </p>
 
         <div className="about-amounts-container">
           <CountBox text="Years of experience" countNumber={1} />
-          <CountBox
-            text="Number of written lines of code"
-            countNumber={10000}
-          />
+          <CountBox text="Number of written lines of code" countNumber={10000} />
           <CountBox text="Hours spent on learning" countNumber={4860} />
         </div>
       </div>
